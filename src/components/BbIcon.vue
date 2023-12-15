35c13517eea2cb06077c6bbd96435ffce6b6ae49
Pavel Karev <pavelkarev88@gmail.com>
2023-12-15 16:27:05 +0100

    Add Icon component

---
+++ src/components/BbIcon.vue
@@ -0,0 +1,18 @@
+<template>
+  <component :is="icon" class="fill-current" />
+</template>
+
+<script setup lang="ts">
+import { defineAsyncComponent } from "vue";
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  }
+});
+
+const icon = defineAsyncComponent(() =>
+    import(/* @vite-ignore */`/src/svgs/${props.name}.svg`)
+);
+</script>
