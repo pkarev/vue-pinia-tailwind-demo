3e5968f0d9de4b2228dd0e2a8528fb1966ddeaf4
Pavel Karev <pavelkarev88@gmail.com>
2023-12-16 14:30:33 +0100

    Add BbDotsButton component

---
+++ src/components/BbDotsButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button type="button" :title="title"
+          class="inline-flex items-center justify-center
+          w-[40px] h-[40px] rounded-[20px]
+          text-gray hover:text-black focus:text-black hover:bg-gray focus:bg-gray"
+  >
+    <BbIcon name="dots" width="22"/>
+  </button>
+</template>
+
+<script setup lang="ts">
+import BbIcon from "./BbIcon.vue";
+
+const {title} = defineProps<{
+  title?: string,
+}>();
+</script>
